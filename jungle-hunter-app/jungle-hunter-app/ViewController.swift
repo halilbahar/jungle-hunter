7d7dc207bc7193be3c0c6bdcbe94fe236dac519a
simonZweimueller <simon.zweimueller1@gmail.com>
2020-02-25 16:16:00 +0100

    create app

---
+++ jungle-hunter-app/jungle-hunter-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jungle-hunter-app
+//
+//  Created by Administrator on 25.02.20.
+//  Copyright © 2020 htl-leonding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
