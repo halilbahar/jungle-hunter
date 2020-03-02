3e8dd8d2d9514fd32fdb914268298e340f0f1ab5
sonjacao <csonja1703@gmail.com>
2020-03-02 11:42:49 +0100

    Create controller to access the camera

--- jungle-hunter-app/jungle-hunter-app/CameraViewController.swift
+++ jungle-hunter-app/jungle-hunter-app/CameraViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraVC = UIImagePickerController()
+            cameraVC.delegate = self
+            cameraVC.allowsEditing = false
+            cameraVC.sourceType = .camera
+            present(cameraVC, animated: true, completion: nil)
+        } else {
+            print("Currently no source type available")
+        }
     }
     
 
