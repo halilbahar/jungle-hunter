8a3db6e3bc85d56f50cf9fa7511f34caeb2bbad2
sonjacao <csonja1703@gmail.com>
2020-04-20 20:32:30 +0200

    Request routes asynchronous

--- jungle-hunter-app/MapViewController.swift
+++ jungle-hunter-app/MapViewController.swift
@@ -8,7 +8,7 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
     @IBOutlet weak var mapView: MKMapView!
     var routes = [Route]()
     var controlpoint: ControlPoint!
-    
+    let routeQueue: DispatchQueue = DispatchQueue.init(label: "MapView-RouteQueue")
     
     @IBAction func searchButton(_ sender: Any) {
         let searchController = UISearchController(searchResultsController: nil)
@@ -25,63 +25,55 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
         self.navigationController?.view.backgroundColor = .clear
         
         self.mapView.delegate = self
-        
-        let url = URL(string:"https://www.mocky.io/v2/5e8317053100004b00e6406c")//Paste HTTP Response url here
-        if let dataUrl = url {
-                let data = try? Data(contentsOf: dataUrl)
-                if let downloadedData = data {
-                    if let jsonObject = try? JSONSerialization.jsonObject(with: downloadedData, options: []) as? [[String:Any]]{
-                        self.routes = jsonObject.map{Route(routeDict: $0)}
-                        print(self.routes)
-                        for trail in self.routes[0].trails {
-                                 var mapTrail = [MKPointAnnotation]()
-                                 for controlpoint in trail.controlpoints {
-                                    let annotation = MKPointAnnotation()
-                                     annotation.title = controlpoint.name
-                                    annotation.coordinate = CLLocationCoordinate2DMake(controlpoint.coordinates[1], controlpoint.coordinates[0])
-                                     
-                                     mapTrail.append(annotation)
-                                 }
-                                 self.mapView.showAnnotations(mapTrail, animated: true)
-                        }
-                        
-                    }
-                    
-                }
-                
+        routeQueue.async {
+            if let routes = Helper.getRoutes() {
+                self.routes = routes
             }
+        }
         
+        /*
+         for trail in self.routes[0].trails {
+         var mapTrail = [MKPointAnnotation]()
+         for controlpoint in trail.controlpoints {
+         let annotation = MKPointAnnotation()
+         annotation.title = controlpoint.name
+         annotation.coordinate = CLLocationCoordinate2DMake(controlpoint.coordinates[0], controlpoint.coordinates[1])
+         
+         mapTrail.append(annotation)
+         }
+         self.mapView.showAnnotations(mapTrail, animated: true)
+         }
+         */
         
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
     }
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         for trail in routes[0].trails {
-             for cpoint in trail.controlpoints {
-                 if(cpoint.name == mapView.selectedAnnotations[0].title!!) {
-                     self.controlpoint = cpoint
-                     break
-                 }
-             }
-         }
-         performSegue(withIdentifier: "controlpointinfo", sender: nil)
-     }
-     
+            for cpoint in trail.controlpoints {
+                if(cpoint.name == mapView.selectedAnnotations[0].title!!) {
+                    self.controlpoint = cpoint
+                    break
+                }
+            }
+        }
+        performSegue(withIdentifier: "controlpointinfo", sender: nil)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         if let controlpointinfoVC = segue.destination as? ControlpointInfoViewController {
-             
-             controlpointinfoVC.controlpoint = controlpoint
-         }
-         
-     }
+        if let controlpointinfoVC = segue.destination as? ControlpointInfoViewController {
+            
+            controlpointinfoVC.controlpoint = controlpoint
+        }
+        
+    }
     
 }
