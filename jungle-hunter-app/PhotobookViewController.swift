a4c3c4e61ad335f75702d8000817addbc526801d
sonjacao <csonja1703@gmail.com>
2020-04-20 21:03:33 +0200

    Format PhotobookViewController.swift

--- jungle-hunter-app/PhotobookViewController.swift
+++ jungle-hunter-app/PhotobookViewController.swift
@@ -1,10 +1,3 @@
-//
-//  PhotobookViewController.swift
-//  jungle-hunter-app
-//
-//  Created by Sonja Cao on 28.02.20.
-//  Copyright © 2020 htl-leonding. All rights reserved.
-//
 
 import UIKit
 
@@ -12,19 +5,5 @@ class PhotobookViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
