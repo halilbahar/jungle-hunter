d1dc769c7f6e43f231281ed572defe1d23d73c06
sonjacao <csonja1703@gmail.com>
2020-04-20 21:10:43 +0200

    Format TrailViewController.swift

--- jungle-hunter-app/TrailViewController.swift
+++ jungle-hunter-app/TrailViewController.swift
@@ -1,10 +1,3 @@
-//
-//  TrailViewController.swift
-//  jungle-hunter-app
-//
-//  Created by Administrator on 24.03.20.
-//  Copyright © 2020 htl-leonding. All rights reserved.
-//
 
 import UIKit
 
@@ -18,8 +11,8 @@ class TrailViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        dataSource = DataSourceTracks()
-        tableView.dataSource = dataSource
+        self.dataSource = DataSourceTracks()
+        self.tableView.dataSource = dataSource
         self.tableView.rowHeight = 60
         
         //Fill Trails
@@ -29,22 +22,10 @@ class TrailViewController: UIViewController {
         self.dataSource.routes = self.routes
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let controlpointVC = segue.destination as? ControlpointViewController {
-            let indexPath = tableView.indexPathForSelectedRow
-            
-            controlpointVC.controlpoints = routes[0].trails[indexPath!.row].controlpoints
+            let indexPath = self.tableView.indexPathForSelectedRow
+            controlpointVC.controlpoints = self.routes[0].trails[indexPath!.row].controlpoints
         }
     }
 }
@@ -55,17 +36,15 @@ class DataSourceTracks: NSObject, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return routes[0].trails.count
+        return self.routes[0].trails.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "trailCell", for: indexPath) as! TableViewCell
-        
-        
-        cell.trail_name.text = routes[0].trails[indexPath.row].name
-        cell.trail_length.text = String(routes[0].trails[indexPath.row].length)
-        
+        cell.trail_name.text = self.routes[0].trails[indexPath.row].name
+        cell.trail_length.text = String(self.routes[0].trails[indexPath.row].length)
         return cell
     }
 }
