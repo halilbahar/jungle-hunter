44088014cd121ba4dad9d895a1c9df7d729254e2
sonjacao <csonja1703@gmail.com>
2020-04-28 10:48:57 +0200

    Create class Coordinate

---
+++ jungle-hunter-app/Coordinate.swift
@@ -0,0 +1,10 @@
+class Coordinate {
+    
+    var latitude: Double
+    var longitude: Double
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
