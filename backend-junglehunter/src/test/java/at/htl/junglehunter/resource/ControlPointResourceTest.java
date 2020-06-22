e3d91616d9eeee15ad1224e07f70821da63b29fa
halilbahar <halil.s.bahar@gmail.com>
2020-06-22 12:23:11 +0200

    Use testcontainer

--- backend-junglehunter/src/test/java/at/htl/junglehunter/resource/ControlPointResourceTest.java
+++ backend-junglehunter/src/test/java/at/htl/junglehunter/resource/ControlPointResourceTest.java
@@ -1,7 +1,12 @@
 package at.htl.junglehunter.resource;
 
+import at.htl.junglehunter.DatabaseResource;
 import com.intuit.karate.junit5.Karate;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 
+@QuarkusTestResource(DatabaseResource.class)
+@QuarkusTest
 public class ControlPointResourceTest {
 
     @Karate.Test
@@ -9,3 +14,4 @@ public class ControlPointResourceTest {
         return Karate.run("control-point").relativeTo(getClass());
     }
 }
+
--- backend-junglehunter/src/test/java/at/htl/junglehunter/resource/RouteResourceTest.java
+++ backend-junglehunter/src/test/java/at/htl/junglehunter/resource/RouteResourceTest.java
@@ -1,7 +1,12 @@
 package at.htl.junglehunter.resource;
 
+import at.htl.junglehunter.DatabaseResource;
 import com.intuit.karate.junit5.Karate;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
 
+@QuarkusTestResource(DatabaseResource.class)
+@QuarkusTest
 public class RouteResourceTest {
 
     @Karate.Test
