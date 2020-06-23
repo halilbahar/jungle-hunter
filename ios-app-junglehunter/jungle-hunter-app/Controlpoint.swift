27f8123f569d9334850706ea1c581b5f8ca19b44
sonjacao <csonja1703@gmail.com>
2020-06-23 09:24:26 +0200

    Remove unnecessary comments

--- ios-app-junglehunter/jungle-hunter-app/Controlpoint.swift
+++ ios-app-junglehunter/jungle-hunter-app/Controlpoint.swift
@@ -1,26 +1,20 @@
 class ControlPoint {
-    //var id: String
     var name: String
-    //var coordinates: [Double]
     var latitude: Double
     var longitude: Double
     var comment: String
     var note: String
     
     init (controlpointDict: [String:Any]) {
-        //self.id = controlpointDict["id"] as! String
         self.name = controlpointDict["name"] as! String
-        //self.coordinates = controlpointDict["coordinates"] as! [Double]
         self.latitude = controlpointDict["latitude"] as! Double
         self.longitude = controlpointDict["longitude"] as! Double
         self.comment = controlpointDict["comment"] as! String
         self.note = controlpointDict["note"] as! String
     }
     
-    init(/*id: String, */name: String, /*coordinates: [Double], */latitude: Double, longitude: Double,comment: String, note: String) {
-        //self.id = id
+    init(name: String, latitude: Double, longitude: Double,comment: String, note: String) {
         self.name = name
-        //self.coordinates = coordinates
         self.latitude = latitude
         self.longitude = longitude
         self.comment = comment
--- ios-app-junglehunter/jungle-hunter-app/Trail.swift
+++ ios-app-junglehunter/jungle-hunter-app/Trail.swift
@@ -1,18 +1,15 @@
 class Trail {
-    //var trailID: String
     var name: String
     var length: Double
     var controlpoints: [ControlPoint]
     
     init (trailDict: [String:Any]) {
-        //self.trailID = trailDict["id"] as! String
         self.name = trailDict["name"] as! String
         self.length = trailDict["length"] as! Double
         self.controlpoints = (trailDict["controlPoints"] as! [[String:Any]]).map{ ControlPoint(controlpointDict: $0) }
     }
     
-    init (/*trailID: String ,*/name: String, length: Double, controlpoints: [ControlPoint]) {
-        //self.trailID = trailID
+    init (name: String, length: Double, controlpoints: [ControlPoint]) {
         self.name = name
         self.length = length
         self.controlpoints = controlpoints
