8ea5000cf81fbc5bd0bf2e9411c07865af7b7f15
halilbahar <halil.s.bahar@gmail.com>
2020-06-23 02:33:40 +0200

    Create temp user

--- backend-junglehunter/src/main/java/at/htl/junglehunter/entity/User.java
+++ backend-junglehunter/src/main/java/at/htl/junglehunter/entity/User.java
@@ -1,13 +1,17 @@
 package at.htl.junglehunter.entity;
 
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.transaction.Transactional;
+import java.util.UUID;
 
 @Entity
 @Table(name = "_user")
@@ -25,4 +29,14 @@ public class User extends PanacheEntity {
 
     public String email;
 
+    @Transactional
+    public static User create(String username, String password) {
+        User user = new User();
+        user.username = UUID.randomUUID().toString().replace("-", "");
+        user.password = BcryptUtil.bcryptHash(RandomStringUtils.random(10));
+        user.role = "user";
+        user.persist();
+
+        return user;
+    }
 }
--- backend-junglehunter/src/main/java/at/htl/junglehunter/resource/UserResource.java
+++ backend-junglehunter/src/main/java/at/htl/junglehunter/resource/UserResource.java
@@ -1,4 +1,33 @@
 package at.htl.junglehunter.resource;
 
+import at.htl.junglehunter.dto.UserDto;
+import at.htl.junglehunter.entity.User;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+@Path("/user")
+@Consumes("application/json")
+@Produces("application/json")
 public class UserResource {
+
+    @GET
+    @Transactional
+    public Response getTempUser() {
+        String secureChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+        String password = RandomStringUtils.random(10, secureChars);
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        User user = User.create(uuid, password);
+        UserDto userDto = UserDto.map(user);
+        userDto.setPassword(password);
+
+        return Response.ok(userDto).build();
+    }
 }
